d6a4e56dc4b85a580cb4ba3228c33214605a9033
Fatou <fatou.binetou.diallo@etu.univ-st-etienne.fr>
2026-05-21 09:03:46 +0200

    fix vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' , textAlign: 'center'}}> 
+    <h1> Mon premier Déploiement </h1> 
+    <p> Projet réalisé par <strong>Fatou Binetou Diallo</strong></p>
+    <p> Futur application : Projet de Taches Kanban</p>
+    </div>
+  );
+}
+export default App
